54c7bacecd12ddefb8b384a086797956e92d7595
unknown <user@g9900-n54967600.gnivc.msk>
2022-10-24 03:26:13 +0300

    first commit

---
+++ pizza-cloud/src/test/java/pizzas/PizzaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package pizzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
