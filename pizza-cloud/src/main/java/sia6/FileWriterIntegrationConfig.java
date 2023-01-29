cfa804a60ca30029f76c0fca8774d8f8dc2dae3e
Pobochnaya <likapro007@yandex.ru>
2023-01-29 22:36:53 +0300

    Интеграция Spring. Определение потока интеграции на Java

---
+++ pizza-cloud/src/main/java/sia6/FileWriterIntegrationConfig.java
@@ -0,0 +1,32 @@
+package sia6;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.integration.transformer.GenericTransformer;
+
+import java.io.File;
+
+@Configuration
+public class FileWriterIntegrationConfig {
+
+    @Bean
+    @Transformer(inputChannel = "textInChannel", outputChannel = "fileWriterChannel") // объявление преобразователя
+    public GenericTransformer<String, String> upperCaseTransformer() {
+        return text -> text.toUpperCase();
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "fileWriterChannel")
+    public FileWritingMessageHandler fileWriter() { // обявление обработчика записи в файл
+        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File("/tmp/sia6/files"));
+        handler.setExpectReply(false);
+        handler.setFileExistsMode(FileExistsMode.APPEND);
+        handler.setAppendNewLine(true);
+
+        return handler;
+    }
+}
