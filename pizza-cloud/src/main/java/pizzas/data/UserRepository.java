d3c808316adfd043a219a4bfae7fcb0230f90c94
Pobochnaya <likapro007@yandex.ru>
2023-01-29 22:13:08 +0300

    Исправление до работающего кода

--- pizza-cloud/src/main/java/pizzas/data/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package pizzas.data;
-
-import org.springframework.data.repository.CrudRepository;
-import pizzas.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-}
